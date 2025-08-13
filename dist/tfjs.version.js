88157bbc4f75cda75ff427e57459bf75b9627806
nine <nine@meerzet.com>
2025-08-13 17:36:15 +0900

    build dist file

--- dist/tfjs.version.js
+++ dist/tfjs.version.js
@@ -4,37 +4,4 @@
   author: <https://github.com/vladmandic>'
 */
 
-
-// node_modules/@tensorflow/tfjs-core/package.json
-var version = "4.22.0";
-
-// node_modules/@tensorflow/tfjs-converter/package.json
-var version2 = "4.22.0";
-
-// node_modules/@tensorflow/tfjs-backend-cpu/package.json
-var version3 = "4.22.0";
-
-// node_modules/@tensorflow/tfjs-backend-webgl/package.json
-var version4 = "4.22.0";
-
-// node_modules/@tensorflow/tfjs-backend-wasm/package.json
-var version5 = "4.22.0";
-
-// node_modules/@tensorflow/tfjs-backend-webgpu/package.json
-var version6 = "4.22.0";
-
-// tfjs/tf-version.ts
-var version7 = {
-  tfjs: version,
-  "tfjs-core": version,
-  // 'tfjs-data': tfjsDataVersion,
-  // 'tfjs-layers': tfjsLayersVersion,
-  "tfjs-converter": version2,
-  "tfjs-backend-cpu": version3,
-  "tfjs-backend-webgl": version4,
-  "tfjs-backend-wasm": version5,
-  "tfjs-backend-webgpu": version6
-};
-export {
-  version7 as version
-};
+var e="4.22.0";var s="4.22.0";var t="4.22.0";var n="4.22.0";var r="4.22.0";var i="4.22.0";var h={tfjs:e,"tfjs-core":e,"tfjs-converter":s,"tfjs-backend-cpu":t,"tfjs-backend-webgl":n,"tfjs-backend-wasm":r,"tfjs-backend-webgpu":i};export{h as version};
